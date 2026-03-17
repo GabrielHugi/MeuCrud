3c51fb70caa4fc536fa200a3360e3d9afb8db0cb
GABRIEL LOUREIRO AMORIM HUGI <gabriel.hugi@etec.sp.gov.br>
2026-03-17 14:03:06 -0300

    isso

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,7 +1,32 @@
-import React, {useEFfect, useState } from "react";
+import React, {useEffect, useEFfect, useState } from "react";
 import { View, Text, FlatList, Button } from "react-native";
 
 import styles from "../styles/styles";
 
 import { getPeople, deletePerson } from "../servers/peopleCrud";
 
+export default function HomeScreen({navigation}) {
+    const [people, setPeople] = useState([]);
+
+    async function loadPeople() {
+        const data = await getPeople();
+        setPeople(data);
+    }
+
+    useEffect(() => {
+        loadPeople();
+    }, []);
+
+    return(
+        <View style={styles.container}>
+
+            <Text style={styles.title}>Pessoa</Text>
+
+            <Button
+                title="Adicionar Pessoa"
+                onPress={() => navigation.navigate}
+            />
+        </View>
+
+    );
+}
